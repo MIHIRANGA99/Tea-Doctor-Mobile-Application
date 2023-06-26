8c8255a5af9b1bbfa4c654362c38f78e25c9e209
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2023-06-26 23:16:56 +0530

    created constants

---
+++ constants/colors.ts
@@ -0,0 +1,6 @@
+export const COLOR_PALETTE = {
+    primary: '#275900',
+    secondary: '#F4F4E0',
+    darker: '#5C6450',
+    lighter: '#92AA75'
+}
---
+++ constants/mainStyles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { COLOR_PALETTE } from './colors';
+
+const mainStyles = StyleSheet.create({
+    main: {
+        backgroundColor: COLOR_PALETTE.secondary,
+        paddingTop: 44,
+        height: '100%'
+    }
+});
+
+export default mainStyles;
