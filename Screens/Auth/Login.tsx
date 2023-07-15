b235eedea949fbcdb682366785a541def7c6d1a8
Mihiranga <kavisha.mihiranga@yahoo.com>
2023-07-15 23:02:51 +0530

    connected firebase

--- Screens/Auth/Login.tsx
+++ Screens/Auth/Login.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, ScrollView, Text } from 'react-native';
 import mainStyles from '../../constants/mainStyles';
 import TextField from '../../Components/TextField/TextField';
 import Button from '../../Components/Button/Button';
 import { Link } from '@react-navigation/native';
 import { COLOR_PALETTE } from '../../constants/colors';
+import { loginUser } from '../../firebase/utils/authentication/authentication';
 
-type Props = {}
+const Login = ({navigation}: {navigation: any}) => {
 
-const Login = (props: Props) => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const onSubmit = async () => {
+        const res = await loginUser(email, password);
+
+        if (res) {
+            console.log(res.response);
+            navigation.navigate('Main');
+        }
+    }
     return (
         <View style={mainStyles.main}>
             <ScrollView>
@@ -21,12 +32,12 @@ const Login = (props: Props) => {
                 >
                     <Image style={{ width: '80%', resizeMode: 'contain' }} source={require("../../assets/tea-doctor-logo.png")} />
                 </View>
-                <View>
-                    <TextField dense placeholder='Your Email Here' label='E - main Address' />
-                    <TextField dense placeholder='Enter Password' label='Password' />
+                <View style={{ paddingVertical: 20 }}>
+                    <TextField onChange={(text) => setEmail(text)} dense placeholder='Your Email Here' label='E - main Address' />
+                    <TextField onChange={(text) => setPassword(text)} dense placeholder='Enter Password' label='Password' />
                     <View style={{ display: 'flex', width: '100%', alignItems: 'flex-end', paddingVertical: 8 }}>
                         <View style={{ width: '40%' }}>
-                            <Button label='Login' />
+                            <Button onClick={() => onSubmit()} label='Login' />
                         </View>
                     </View>
                 </View>
