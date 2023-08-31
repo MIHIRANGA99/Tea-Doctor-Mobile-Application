56fcc9794430bcb518b2b9167d3d244850931c3b
Mihiranga Goonetilleke <mgoonetilleke@icptechno.com>
2023-08-31 20:21:16 +0530

    created tree management

--- Screens/MyPlants/TreeDetails.tsx
+++ Screens/MyPlants/TreeDetails.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image, ScrollView, Text } from "react-native";
 import DetailCard from "../../Components/DetailCard/DetailCard";
 import Button from "../../Components/Button/Button";
 import mainStyles from "../../constants/mainStyles";
 import { COLOR_PALETTE } from "../../constants/colors";
+import useCurrentUser from "../../firebase/hooks/useCurrentUser";
+import { deleteFromCollection } from "../../firebase/utils/firestore/firestore";
 
-const TreeDetails = ({ route, navigation }: { route: any, navigation: any }) => {
+const TreeDetails = ({
+  route,
+  navigation,
+}: {
+  route: any;
+  navigation: any;
+}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const currentUser = useCurrentUser();
+
+  const handleDelete = () => {
+    if (currentUser) {
+      setIsLoading(true);
+      deleteFromCollection(
+        currentUser.uid,
+        route.params.id,
+        (res) => {
+          setIsLoading(false);
+          navigation.pop();
+        },
+        (error) => {
+          setIsLoading(false);
+          console.error(error)
+        }
+      );
+    }
+  };
   return (
     <ScrollView style={mainStyles.main}>
       <View
@@ -21,7 +50,16 @@ const TreeDetails = ({ route, navigation }: { route: any, navigation: any }) => 
         />
       </View>
       <View style={{ paddingVertical: 12 }}>
-        <Text style={{ textAlign: 'center', fontSize: 16, fontWeight: '600', color: COLOR_PALETTE.primary }}>{route.params.name}</Text>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 16,
+            fontWeight: "600",
+            color: COLOR_PALETTE.primary,
+          }}
+        >
+          {route.params.name}
+        </Text>
       </View>
       <DetailCard
         header="Suggestions"
@@ -32,29 +70,35 @@ const TreeDetails = ({ route, navigation }: { route: any, navigation: any }) => 
           header="Condition of Leaves"
           description="Check the tea leaves"
           button={{
-            label: 'Rescan Leaves',
-            onClick: () => navigation.navigate('Scan', {scanType: 'blister'}),
-            icon: require('../../assets/icons/eco.png')
+            label: "Rescan Leaves",
+            onClick: () => navigation.navigate("Scan", { scanType: "blister" }),
+            icon: require("../../assets/icons/eco.png"),
           }}
         />
         <DetailCard
           header="Condition of Branches"
           button={{
-            label: 'Scan Branches',
-            onClick: () => navigation.navigate('Scan', {scanType: 'stem'}),
-            icon: require('../../assets/icons/eco.png')
-          }} />
+            label: "Scan Branches",
+            onClick: () => navigation.navigate("Scan", { scanType: "stem" }),
+            icon: require("../../assets/icons/eco.png"),
+          }}
+        />
         <DetailCard
           header="Existence of Bugs"
           button={{
-            label: 'Scan Bugs',
-            onClick: () => navigation.navigate('Bugs', {scanType: 'insect'}),
-            icon: require('../../assets/icons/bug_report.png')
+            label: "Scan Bugs",
+            onClick: () => navigation.navigate("Bugs", { scanType: "insect" }),
+            icon: require("../../assets/icons/bug_report.png"),
           }}
         />
       </View>
       <View style={{ paddingVertical: 12 }}>
-        <Button onClick={() => null} extraStyles={{ backgroundColor: '#AD0000' }} label="Delete Tree" />
+        <Button
+          isLoading = {isLoading}
+          onClick={() => handleDelete()}
+          extraStyles={{ backgroundColor: "#AD0000" }}
+          label="Delete Tree"
+        />
       </View>
     </ScrollView>
   );
