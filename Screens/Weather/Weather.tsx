bfce1867b0f3662558b952554ba454e5bba780ac
Mihiranga Goonetilleke <mgoonetilleke@icptechno.com>
2023-09-02 21:22:09 +0530

    weather next previous completed

--- Screens/Weather/Weather.tsx
+++ Screens/Weather/Weather.tsx
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 import DetailCard from "../../Components/DetailCard/DetailCard";
 import mainStyles from "../../constants/mainStyles";
 import axios from "axios";
 import useCurrentLocation from "../../hooks/useCurrentLocation";
 import useCurrentUser from "../../firebase/hooks/useCurrentUser";
 import FullScreenLoader from "../../layouts/FullScreenLoader";
+import { COLOR_PALETTE } from "../../constants/colors";
+import AnimatedLottieView from "lottie-react-native";
 
 const Weather = ({ changeTab }: { changeTab: (number: number) => void }) => {
   const [weatherData, setWeatherData] = useState<any>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [day, setDay] = useState<Date>(new Date());
   const currentLocation = useCurrentLocation();
   const currentUser = useCurrentUser();
 
   useEffect(() => {
+    getWeatherData(new Date().toLocaleDateString());
+  }, [currentUser, currentLocation]);
+
+  let formattedDate = null;
+  if (weatherData) {
+    const detectionDate = new Date(weatherData.detection_date);
+    formattedDate = detectionDate.toLocaleDateString();
+  }
+
+  const handleNextDate = () => {
+    const date = day;
+    date.setDate(day.getDate() + 1);
+    getWeatherData(date.toLocaleDateString());
+  };
+
+  const handlePreviousDate = () => {
+    const date = day;
+    date.setDate(day.getDate() - 1);
+    getWeatherData(date.toLocaleDateString());
+  };
+
+  const getWeatherData = (dateString: string) => {
     if (currentLocation && currentUser) {
       setIsLoading(true);
       axios
         .get(
-          "http://api.weatherapi.com/v1/current.json?key=adbdbab1c56947b0b6194257230109&q=Colombo"
+          `https://weatherapi-com.p.rapidapi.com/current.json?q=${currentLocation.coords.latitude},${currentLocation.coords.longitude}`,
+          {
+            headers: {
+              "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+              "X-RapidAPI-Key":
+                "3623898063msh6d53471e69619bcp1c3645jsn69245bfec7d6",
+            },
+          }
         )
         .then((res) => {
           const weather = {
@@ -29,7 +61,7 @@ const Weather = ({ changeTab }: { changeTab: (number: number) => void }) => {
             temp_max: res.data.current.temp_c,
             temp_min: res.data.current.temp_c,
             wind: res.data.current.wind_kph,
-            today: new Date().toLocaleDateString(),
+            today: dateString,
           };
           axios
             .post("http://3.112.233.148:8091/detection/detect-weather", weather)
@@ -48,13 +80,7 @@ const Weather = ({ changeTab }: { changeTab: (number: number) => void }) => {
           setIsLoading(false);
         });
     }
-  }, [currentUser, currentLocation]);
-
-  let formattedDate = null;
-  if (weatherData) {
-    const detectionDate = new Date(weatherData.detection_date);
-    formattedDate = detectionDate.toLocaleDateString();
-  }
+  };
 
   return (
     <View style={mainStyles.main}>
@@ -74,56 +100,110 @@ const Weather = ({ changeTab }: { changeTab: (number: number) => void }) => {
         header="Suggestions"
         description="Check the tea leaves and scan if you see any odd spots"
       />
-      {/* <FullScreenLoader isLoading={isLoading}> */}
+      <FullScreenLoader isLoading={isLoading}>
         <View style={{ paddingVertical: 12, height: isLoading ? 200 : "100%" }}>
-          <DetailCard header="Weather on Rathganga" />
           {weatherData && (
-            <View style={styles.container}>
-              <Text style={styles.dateText}>Date: {formattedDate}</Text>
-              <View style={styles.infoContainer}>
-                <View style={styles.infoRow}>
-                  <Text style={styles.label}>Temperature:</Text>
-                  <Text style={styles.value}>{weatherData.temps[0]} °C</Text>
+            <DetailCard header="Weather on Rathganga">
+              <View style={styles.container}>
+                <Text style={styles.dateText}>Date: {formattedDate}</Text>
+                <View style={styles.infoContainer}>
+                  <View style={styles.infoRow}>
+                    <Text style={styles.label}>Today's Weather:</Text>
+                    <Text style={styles.value}>
+                      {weatherData.todayWeatherClass}
+                    </Text>
+                  </View>
+                  <View style={styles.infoRow}>
+                    <Text style={styles.label}>Temperature:</Text>
+                    <Text style={styles.value}>{weatherData.temps[0]} °C</Text>
+                  </View>
+                  <View style={styles.infoRow}>
+                    <Text style={styles.label}>Humidity:</Text>
+                    <Text style={styles.value}>
+                      {weatherData.humidities[0]} %
+                    </Text>
+                  </View>
+                  <View style={styles.infoRow}>
+                    <Text style={styles.label}>Rainfalls:</Text>
+                    <Text style={styles.value}>
+                      {weatherData.rainfalls[0]} mm
+                    </Text>
+                  </View>
+                  <View style={styles.infoRow}>
+                    <Text style={styles.label}>Wind Speed:</Text>
+                    <Text style={styles.value}>{weatherData.wind} km/h</Text>
+                  </View>
                 </View>
-                <View style={styles.infoRow}>
-                  <Text style={styles.label}>Humidity:</Text>
-                  <Text style={styles.value}>
-                    {weatherData.humidities[0]} %
-                  </Text>
-                </View>
-                <View style={styles.infoRow}>
-                  <Text style={styles.label}>Rainfalls:</Text>
-                  <Text style={styles.value}>
-                    {weatherData.rainfalls[0]} mm
-                  </Text>
-                </View>
-                <View style={styles.infoRow}>
-                  <Text style={styles.label}>Wind Speed:</Text>
-                  <Text style={styles.value}>{weatherData.wind} km/h</Text>
-                </View>
-                <View style={styles.infoRow}>
-                  <Text style={styles.label}>Today's Weather:</Text>
-                  <Text style={styles.value}>
-                    {weatherData.todayWeatherClass}
-                  </Text>
-                </View>
+                <TouchableOpacity
+                  onPress={() => handleNextDate()}
+                  style={{
+                    position: "absolute",
+                    backgroundColor: COLOR_PALETTE.secondary,
+                    borderColor: COLOR_PALETTE.primary,
+                    borderWidth: 2,
+                    padding: 8,
+                    borderRadius: 100,
+                    top: "50%",
+                    right: -24,
+                    zIndex: 50,
+                  }}
+                >
+                  <AnimatedLottieView
+                    style={{ height: 30, width: 30 }}
+                    source={require("../../assets/lotties/next.json")}
+                    autoPlay
+                    loop
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => handlePreviousDate()}
+                  style={{
+                    position: "absolute",
+                    backgroundColor: COLOR_PALETTE.secondary,
+                    borderColor: COLOR_PALETTE.primary,
+                    borderWidth: 2,
+                    padding: 8,
+                    borderRadius: 100,
+                    top: "50%",
+                    left: -24,
+                    zIndex: 50,
+                  }}
+                >
+                  <AnimatedLottieView
+                    style={{
+                      height: 30,
+                      width: 30,
+                      transform: [{ rotate: "90deg" }],
+                    }}
+                    source={require("../../assets/lotties/next.json")}
+                    autoPlay
+                    loop
+                  />
+                </TouchableOpacity>
               </View>
-            </View>
+            </DetailCard>
           )}
         </View>
-      {/* </FullScreenLoader> */}
+      </FullScreenLoader>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: COLOR_PALETTE.secondary,
+    position: "relative",
+    paddingVertical: 20,
+    paddingHorizontal: 40,
     borderRadius: 10,
     margin: 10,
   },
   dateText: {
+    color: COLOR_PALETTE.secondary,
+    backgroundColor: COLOR_PALETTE.primary,
+    padding: 12,
+    textAlign: "center",
+    borderRadius: 12,
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 10,
@@ -139,6 +219,7 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   label: {
+    color: COLOR_PALETTE.primary,
     fontSize: 16,
     fontWeight: "bold",
   },
