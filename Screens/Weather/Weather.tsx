0271b1eca12df8f151b12a7f25654960159f2c1b
chethuuu <it20065294@my.sliit.lk>
2023-09-02 19:29:22 +0530

    feat: retrive weather details

--- Screens/Weather/Weather.tsx
+++ Screens/Weather/Weather.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Image, ScrollView } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 import DetailCard from "../../Components/DetailCard/DetailCard";
 import mainStyles from "../../constants/mainStyles";
 import axios from "axios";
@@ -35,17 +35,26 @@ const Weather = ({ changeTab }: { changeTab: (number: number) => void }) => {
             .post("http://3.112.233.148:8091/detection/detect-weather", weather)
             .then((response) => {
               console.log(response.data);
+              setWeatherData(response.data.data);
               setIsLoading(false);
             })
             .catch((error) => {
               console.error(error);
+              setIsLoading(false);
             });
         })
         .catch((error) => {
           console.log(error);
+          setIsLoading(false);
         });
     }
   }, [currentUser, currentLocation]);
+
+  let formattedDate = null;
+  if (weatherData) {
+    const detectionDate = new Date(weatherData.detection_date);
+    formattedDate = detectionDate.toLocaleDateString();
+  }
 
   return (
     <View style={mainStyles.main}>
@@ -65,13 +74,77 @@ const Weather = ({ changeTab }: { changeTab: (number: number) => void }) => {
         header="Suggestions"
         description="Check the tea leaves and scan if you see any odd spots"
       />
-      <FullScreenLoader isLoading={isLoading}>
+      {/* <FullScreenLoader isLoading={isLoading}> */}
         <View style={{ paddingVertical: 12, height: isLoading ? 200 : "100%" }}>
-          <DetailCard header="Weather on Panadura" />
+          <DetailCard header="Weather on Rathganga" />
+          {weatherData && (
+            <View style={styles.container}>
+              <Text style={styles.dateText}>Date: {formattedDate}</Text>
+              <View style={styles.infoContainer}>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Temperature:</Text>
+                  <Text style={styles.value}>{weatherData.temps[0]} °C</Text>
+                </View>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Humidity:</Text>
+                  <Text style={styles.value}>
+                    {weatherData.humidities[0]} %
+                  </Text>
+                </View>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Rainfalls:</Text>
+                  <Text style={styles.value}>
+                    {weatherData.rainfalls[0]} mm
+                  </Text>
+                </View>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Wind Speed:</Text>
+                  <Text style={styles.value}>{weatherData.wind} km/h</Text>
+                </View>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Today's Weather:</Text>
+                  <Text style={styles.value}>
+                    {weatherData.todayWeatherClass}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          )}
         </View>
-      </FullScreenLoader>
+      {/* </FullScreenLoader> */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    margin: 10,
+  },
+  dateText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  infoContainer: {
+    backgroundColor: "#f5f5f5",
+    borderRadius: 8,
+    padding: 10,
+  },
+  infoRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 8,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  value: {
+    fontSize: 16,
+  },
+});
 
 export default Weather;
